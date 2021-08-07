f034daf543f2b736f6ed05ae6b0ec497f7cb698a
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-08-06 21:04:34 -0400

    Start: Create new files

---
+++ models.py
@@ -0,0 +1,29 @@
+"""Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to Database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                    primary_key = True,
+                    autoincrement = True)
+    
+    first_name = db.Column(db.String(50),
+                    nullable = False)
+
+    last_name = db.Column(db.String(50),
+                    nullable = False)
+
+    img_url = db.Column(db.Text,
+                    default = 'https://www.cpd-umanitoba.com/wp-content/uploads/2016/08/profile-missing.png')
